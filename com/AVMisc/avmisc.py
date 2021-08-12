4807bfe66df2ec7b6a50873435943f94bb046972
Darren Thompson <darren.thompson200@gmail.com>
2021-08-12 04:47:22 -0400

    Add help menu and more avmisc tests

--- com/AVMisc/avmisc.py
+++ com/AVMisc/avmisc.py
@@ -1,11 +1,13 @@
 """The entry point for AVMisc."""
 import sys
 from enum import Enum
+from rich import print
 import audio_utils
 
 class Command(Enum):
   LIST_DEFAULT_AUDIO_DEVICES = 'list_def_audio_dev'
   LIST_ALL_AUDIO_DEVICES = 'list_all_audio_dev'
+  HELP = 'help'
   EXIT = 'exit'
 
 COMMANDS = {
@@ -20,26 +22,47 @@ COMMANDS = {
   Command.EXIT: {
     'description': 'Exit the program.',
     'method': sys.exit
+  },
+  Command.HELP: {
+    'description': 'Show this help message.',
+    'method': None
   }
 }
+
+def _print_help() -> None:
+  print('[bold red]------------------------\n'
+        '[bold red]AVMISC OPTIONS\n'
+        '[bold red]------------------------\n')
+  for command, data in COMMANDS.items():
+    print(f'\'{command.value}\': {data["description"]}\n')
 
 def process_command(command: str) -> None:
   """Runs the method associated with the given command if it is valid.
 
   Args:
     command: The name of the command to run.
+
+  Raises:
+    ValueError: If an invalid command is given.
   """
-  try:
+  if command in [member.value for member in Command.__members__.values()]:
     COMMANDS[Command(command)]['method']()
-  except ValueError:
-    print(f'\'{command}\' is not a recognized command. Type \'help\' for ' +
-      'a list of available commands.')
+  else:
+    raise ValueError
 
 def begin() -> None:
   """Serves as the entry method for AVMisc."""
   while True:
-    command = input('avmisc>')
-    process_command(command)
+    print('[bold blue]avmisc>', end=' ')
+    command = input('')
+    try:
+      if command == Command.HELP.value:
+        _print_help()
+      else:
+        process_command(command)
+    except ValueError:
+      print(f'\'{command}\' is not a recognized command. Type \'help\' for ' +
+      'a list of available commands.')
 
 
 if __name__ == '__main__':
--- com/AVMisc/avmisc_test.py
+++ com/AVMisc/avmisc_test.py
@@ -1,16 +1,17 @@
 """Tests for avmisc.py."""
 
-import pytest # pylint: disable=unused-import
+import pytest
 import avmisc
+from avmisc import Command, COMMANDS
 
 class TestAVMisc():
-  def testCommandDictValidity(self):
+  def testCommandDictIsValid(self):
     """Validates the entries in the command dictionary.
 
     Makes sure each entry in the command dictionary has a description
     and method.
     """
-    for command_info in avmisc.COMMANDS.values():
+    for command_info in COMMANDS.values():
       assert 'description' in command_info
       assert 'method' in command_info
       assert command_info['description'] != ''
@@ -18,8 +19,22 @@ class TestAVMisc():
   def testCommandEnumMatchesCommandDict(self):
     """Compares the Command enum to the command dictionary.
 
-    Makes sure each entry in the Command enum is used in the
-    command dictionary.
+    Makes sure each entry in the Command enum exists in the command
+    dictionary and vice versa.
     """
-    for command in avmisc.Command:
-      assert command in avmisc.COMMANDS
+    for command in Command:
+      assert command in COMMANDS
+
+    for command in COMMANDS:
+      assert command in Command
+
+  def testProcessCommandShouldRaiseError(self):
+    with pytest.raises(ValueError):
+      avmisc.process_command('foo')
+
+  def testProcessCommandShouldNotRaiseError(self):
+    try:
+      avmisc.process_command(Command.LIST_ALL_AUDIO_DEVICES.value)
+      assert True
+    except ValueError:
+      assert False, 'process_command failed unexpectedly.'
