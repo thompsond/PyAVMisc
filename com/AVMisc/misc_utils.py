03e0f6f3a7bcac131fc6ef8b59d6787cf397a064
Darren Thompson <darren.thompson200@gmail.com>
2021-08-16 23:22:07 -0400

    Fix compatibility for text-to-speech

--- com/AVMisc/misc_utils.py
+++ com/AVMisc/misc_utils.py
@@ -4,18 +4,21 @@ from random import seed, randint
 from time import sleep
 import pyautogui
 from common_utils import get_formatted_date
+from gtts import gTTS
+from playsound import playsound
 import pyperclip
-import pyttsx3
 
 def take_screenshot() -> None:
   """Take a screenshot.
 
   This is not fully supported on Linux.
   """
-  pyautogui.screenshot(f'Screenshot_{get_formatted_date()}.png')
+  file_path = f'Screenshot_{get_formatted_date()}.png'
+  pyautogui.screenshot(file_path)
+  print(f'Screenshot saved to {file_path}.')
 
-def get_clipboard_contents() -> str:
-  return pyperclip.paste() or 'Nothing on the clipboard.'
+def get_clipboard_contents() -> None:
+  print(pyperclip.paste() or 'Nothing on the clipboard.')
 
 def start_cursor_jump() -> None:
   """Make the cursor move to random locations on the screen."""
@@ -31,7 +34,8 @@ def start_cursor_jump() -> None:
 def speak_text() -> None:
   """Use an automated voice to speak the text given by the user."""
   text = input('Enter the text to speak: ')
-  engine = pyttsx3.init()
-  engine.setProperty('rate', 125)
-  engine.say(text)
-  engine.runAndWait()
+  tts_obj = gTTS(text=text, lang='en', slow=False)
+  file_path = f'Text_{get_formatted_date()}.mp3'
+  tts_obj.save(file_path)
+  print(f'Speech saved to {file_path}.')
+  playsound(file_path)
